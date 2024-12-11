eaa85d6466e147ca0bf4cce6ec53971086bfa468
AhmedMaged <osans026@gmail.com>
2024-12-11 23:41:13 +0200

    added main menu and fixing some client's sync issues

--- src/animation/animation.js
+++ src/animation/animation.js
@@ -7,79 +7,95 @@ it's determined by using <characterName>_<state>_<facingRightOrLeft>
 // animations are preloaded in bootloader.js
 
 export class Anim {
-  constructor(scene, gameObjName) {
+  constructor(id, scene, gameObjName) {
+    this.id = id;
     this.name = gameObjName;
     this.scene = scene;
   }
 
   createAnim() {
     this.scene.anims.create({
-      key: "run right",
+      key: `${this.id} run right`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_run_right`, {
         start: 0,
-        end: 10,
+        end:
+          this.scene.textures.get(`${this.name}_run_right`).getFrameNames()
+            .length - 1,
       }),
       frameRate: 20,
       repeat: -1,
     });
 
     this.scene.anims.create({
-      key: "run left",
+      key: `${this.id} run left`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_run_left`, {
-        start: 0,
-        end: 11,
+        start:
+          this.scene.textures.get(`${this.name}_run_left`).getFrameNames()
+            .length - 1,
+        end: 0,
       }),
       frameRate: 20,
       repeat: -1,
     });
 
     this.scene.anims.create({
-      key: "idle right",
+      key: `${this.id} idle right`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_idle_right`, {
         start: 0,
-        end: 10,
+        end:
+          this.scene.textures.get(`${this.name}_idle_right`).getFrameNames()
+            .length - 1,
       }),
       frameRate: 5,
       repeat: -1,
     });
 
     this.scene.anims.create({
-      key: "idle left",
+      key: `${this.id} idle left`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_idle_left`, {
-        start: 0,
-        end: 10,
+        start:
+          this.scene.textures.get(`${this.name}_idle_left`).getFrameNames()
+            .length - 1,
+        end: 0,
       }),
       frameRate: 5,
       repeat: -1,
     });
 
     this.scene.anims.create({
-      key: "jump right",
+      key: `${this.id} jump right`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_jump_right`, {
         start: 0,
-        end: 9,
+        end:
+          this.scene.textures.get(`${this.name}_jump_right`).getFrameNames()
+            .length - 1,
       }),
       frameRate: 10,
       repeat: 0,
     });
 
     this.scene.anims.create({
-      key: "jump left",
+      key: `${this.id} jump left`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_jump_left`, {
-        start: 0,
-        end: 9,
+        start:
+          this.scene.textures.get(`${this.name}_jump_left`).getFrameNames()
+            .length - 1,
+        end: 0,
       }),
       frameRate: 10,
       repeat: 0,
     });
 
     this.scene.anims.create({
-      key: "dbljump right",
+      key: `${this.id} dbljump right`,
       frames: this.scene.anims.generateFrameNumbers(
         `${this.name}_dbljump_right`,
         {
           start: 0,
-          end: 9,
+          end:
+            this.scene.textures
+              .get(`${this.name}_dbljump_right`)
+              .getFrameNames().length - 1,
         }
       ),
       frameRate: 10,
@@ -87,12 +103,14 @@ export class Anim {
     });
 
     this.scene.anims.create({
-      key: "dbljump left",
+      key: `${this.id} dbljump left`,
       frames: this.scene.anims.generateFrameNumbers(
         `${this.name}_dbljump_left`,
         {
-          start: 0,
-          end: 9,
+          start:
+            this.scene.textures.get(`${this.name}_dbljump_left`).getFrameNames()
+              .length - 1,
+          end: 0,
         }
       ),
       frameRate: 10,
@@ -100,40 +118,48 @@ export class Anim {
     });
 
     this.scene.anims.create({
-      key: "shot left",
+      key: `${this.id} shot left`,
       frames: this.scene.anims.generateFrameNumbers(`${this.name}_shot_left`, {
-        start: 0,
-        end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.scene.anims.create({
-      key: "shot right",
-      frames: this.scene.anims.generateFrameNumbers(`${this.name}_shot_right`, {
-        start: 0,
-        end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.scene.anims.create({
-      key: "hurt right",
-      frames: this.scene.anims.generateFrameNumbers(`${this.name}_hurt_right`, {
-        start: 0,
-        end: 4,
+        start:
+          this.scene.textures.get(`${this.name}_shot_left`).getFrameNames()
+            .length - 1,
+        end: 0,
       }),
       frameRate: 10,
       repeat: 0,
     });
 
     this.scene.anims.create({
-      key: "hurt left",
-      frames: this.scene.anims.generateFrameNumbers(`${this.name}_hurt_left`, {
+      key: `${this.id} shot right`,
+      frames: this.scene.anims.generateFrameNumbers(`${this.name}_shot_right`, {
         start: 0,
-        end: 4,
+        end:
+          this.scene.textures.get(`${this.name}_shot_right`).getFrameNames()
+            .length - 1,
+      }),
+      frameRate: 10,
+      repeat: 0,
+    });
+
+    this.scene.anims.create({
+      key: `${this.id} hurt right`,
+      frames: this.scene.anims.generateFrameNumbers(`${this.name}_hurt_right`, {
+        start: 0,
+        end:
+          this.scene.textures.get(`${this.name}_hurt_right`).getFrameNames()
+            .length - 1,
+      }),
+      frameRate: 10,
+      repeat: 0,
+    });
+
+    this.scene.anims.create({
+      key: `${this.id} hurt left`,
+      frames: this.scene.anims.generateFrameNumbers(`${this.name}_hurt_left`, {
+        start:
+          this.scene.textures.get(`${this.name}_hurt_left`).getFrameNames()
+            .length - 1,
+        end: 0,
       }),
       frameRate: 10,
       repeat: 0,
--- src/player/player.js
+++ src/player/player.js
@@ -1,61 +1,75 @@
 import { Anim } from "../animation/animation.js";
 
 export class Player extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, state, name) {
+  constructor(scene, playerID, x, y, state, name, fireCooldown) {
     super(scene, x, y);
     this.scene = scene;
     this.name = name;
+    this.id = playerID;
     this.currentState = state;
-    this.animation = new Anim(this.scene, this.name);
-    scene.add.existing(this); // add player to the scene
-    scene.physics.add.existing(this); // add physics to the player
-    this.doubleJump = false; // to detect the player did only a one double jump
-    this.fireCooldown = 500; // 500ms cooldown between shots
-    this.lastFireTime = 0; // Timestamp of the last fire
-    this.fireSound = this.scene.sound.add("fire_sound");
+    this.animation = new Anim(this.id, this.scene, this.name);
+    this.fireCooldown = fireCooldown; // cooldown between shots
     this.init();
   }
   init() {
+    this.animation.createAnim();
+    this.scene.add.existing(this); // add player to the scene
+    this.scene.physics.add.existing(this); // add physics to the player
+    this.isDoubleJumped = false; // to detect the player did only a one double jump
+    this.fireSound = this.scene.sound.add(`${this.name}_gun_sound`); // sound of the gun
     this.body.setCollideWorldBounds(true);
     this.cursor = this.scene.input.keyboard.createCursorKeys();
     this.keys = this.scene.input.keyboard.addKeys({
       f: Phaser.Input.Keyboard.KeyCodes.F,
     });
-    this.animation.createAnim();
+
+    this.on("animationstart", (animation) => {
+      // get the timestap of the fire animation
+
+      if (
+        animation.key.split(" ")[1] == "shot" &&
+        this.scene.socket.id == this.id
+      )
+        this.startAnimationtime = this.scene.time.now;
+    });
+
+    this.on("animationupdate", (animation) => {
+      // to apply the cooldown between shots
+      const elapsedTime = this.scene.time.now - this.startAnimationtime;
+      if (animation.key.split(" ")[1] == "shot") {
+        if (elapsedTime >= this.fireCooldown && this.startAnimationtime) {
+          const facingLeft = this.currentState.split(" ")[1] == "left" ? -1 : 1; //direction determination
+          this.startAnimationtime = null;
+
+          this.createBullet(
+            // dont create a bullet if it's the shotgun player (Raider_1) (make the bullet go out of boundries)
+            this.name != "Raider_1" ? this.x + 20 * facingLeft : facingLeft, // adjust the bullet init position (so it looks as if it's coming out from the gun)
+            this.name != "Raider_1"
+              ? this.y + (this.name == "Gangsters_2" ? 10 : 25)
+              : -1,
+            2000 * facingLeft
+          );
+        }
+      }
+    });
   }
-  updateMovement(time) {
+  updateMovement() {
     if (
-      this.anims.currentAnim.key.split(" ")[0] === "hurt" &&
-      this.anims.isPlaying
+      this.anims.isPlaying &&
+      this.anims.currentAnim.key.split(" ")[1] === "hurt"
     )
       return; //if its the hurt animation then it must finish playing first before any other animation
     if (!this.body.touching.down) {
-      if (this.cursor.up.isDown && this.doubleJump) {
-        this.doubleJump = false;
-        this.body.setVelocityY(-300);
-        this.currentState = `dbljump ${this.currentState.split(" ")[1]}`;
-      }
-      if (this.cursor.right.isDown) {
-        this.body.setVelocityX(400);
-        this.currentState = `${
-          this.currentState.split(" ")[0] == "dbljump" ? "dbljump" : "jump" // while in air , we either set and play jump or double jump animation
-        } right`;
-      } else if (this.cursor.left.isDown) {
-        this.body.setVelocityX(-400);
-        this.currentState = `${
-          this.currentState.split(" ")[0] == "dbljump" ? "dbljump" : "jump"
-        } left`;
-      } else {
-        // being idle in air
-        this.body.setVelocityX(0);
-        this.currentState = `${
-          this.currentState.split(" ")[0] == "dbljump" ? "dbljump" : "jump"
-        } ${this.currentState.split(" ")[1]}`;
-      }
+      if (this.cursor.up.isDown && this.isDoubleJumped) this.doubleJump();
+
+      if (this.cursor.right.isDown) this.moveRightInAir();
+      else if (this.cursor.left.isDown) this.moveLeftInAir();
+      else this.idleInAir();
+
       this.playAnim(this.currentState);
     } else {
-      this.doubleJump = true;
-      if (this.keys.f.isDown) this.Fire(time);
+      this.isDoubleJumped = true;
+      if (this.keys.f.isDown) this.Fire();
       else if (this.cursor.right.isDown) {
         if (this.cursor.up.isDown) this.jumpRight();
         else this.runRight();
@@ -68,7 +82,7 @@ export class Player extends Phaser.GameObjects.Sprite {
   }
 
   playAnim(key) {
-    this.anims.play(key, true);
+    this.anims.play(`${this.id} ${key}`, true);
   }
 
   runRight() {
@@ -109,20 +123,38 @@ export class Player extends Phaser.GameObjects.Sprite {
     this.playAnim(this.currentState);
   }
 
-  Fire(time) {
+  doubleJump() {
+    this.isDoubleJumped = false;
+    this.body.setVelocityY(-300);
+    this.currentState = `dbljump ${this.currentState.split(" ")[1]}`;
+  }
+
+  moveRightInAir() {
+    this.body.setVelocityX(400);
+    this.currentState = `${
+      this.currentState.split(" ")[0] == "dbljump" ? "dbljump" : "jump" // while in air , we either set and play jump or double jump animation
+    } right`;
+  }
+
+  moveLeftInAir() {
+    this.body.setVelocityX(-400);
+    this.currentState = `${
+      this.currentState.split(" ")[0] == "dbljump" ? "dbljump" : "jump" // while in air , we either set and play jump or double jump animation
+    } left`;
+  }
+
+  idleInAir() {
+    // being idle in air
+    this.body.setVelocityX(0);
+    this.currentState = `${
+      this.currentState.split(" ")[0] == "dbljump" ? "dbljump" : "jump"
+    } ${this.currentState.split(" ")[1]}`;
+  }
+
+  Fire() {
     this.body.setVelocityX(0);
     this.currentState = `shot ${this.currentState.split(" ")[1]}`;
     this.playAnim(this.currentState);
-    if (time - this.lastFireTime > this.fireCooldown) {
-      const facingLeft = this.currentState.split(" ")[1] == "left" ? -1 : 1; //if facing left, make the velocity negative
-      this.createBullet(
-        this.x + 25 * facingLeft, // adjust the bullet init position (so it looks as if it's coming out from the gun)
-        this.y + 25,
-        2000 * facingLeft
-      );
-      this.fireSound.play();
-      this.lastFireTime = time;
-    }
   }
 
   createBullet(x, y, velocityX) {
@@ -131,7 +163,7 @@ export class Player extends Phaser.GameObjects.Sprite {
       x: x,
       y: y,
       velocityX: velocityX,
-      srcID: this.scene.socket.id,
+      srcID: this.id,
     });
   }
 
